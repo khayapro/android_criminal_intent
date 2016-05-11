5505e09751b24750938107818c8bb8dbc63e0b28
khayapro <kmathebula@fnb.co.za>
2016-05-11 19:27:49 +0200

    disabled date button and added event handler for checkbox resolved

--- src/main/java/criminal/com/criminalintent/controllers/CrimeFragment.java
+++ src/main/java/criminal/com/criminalintent/controllers/CrimeFragment.java
@@ -9,6 +9,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,6 +23,9 @@ public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
     private TextView mTitleView;
+    private Button mDateButton;
+    private CheckBox mResolved;
+
 
 
     public Crime getCrime() {
@@ -40,8 +46,21 @@ public class CrimeFragment extends Fragment {
         // Inflate the layout for this fragment
         final View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
-        mTitleField = ((EditText) v.findViewById(R.id.crime_title));
+        mDateButton = ((Button) v.findViewById(R.id.crime_date));
+        mDateButton.setEnabled(false);
+        mDateButton.setText(mCrime.getTitle());
+
+        mResolved = ((CheckBox) v.findViewById(R.id.crime_resolved));
+        mResolved.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //setting crime if resolved or not.
+                mResolved.setChecked(isChecked);
+            }
+        });
+
         mTitleView = ((TextView) v.findViewById(R.id.title_view));
+        mTitleField = ((EditText) v.findViewById(R.id.crime_title));
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
